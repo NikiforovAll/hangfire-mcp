9fc20eea5dc97cdcbcb2f3bd2b80514fa390a339
Oleksii Nikiforov <oleksii_nikiforov@epam.com>
2025-05-24 19:03:55 +0300

    feat: dynamic job scheduling MVP

--- samples/Web/HangfireDynamicScheduler.cs
+++ samples/Web/HangfireDynamicScheduler.cs
@@ -2,53 +2,212 @@ namespace Web;
 
 using System.Linq.Expressions;
 using System.Reflection;
-using System.Threading.Tasks;
+using System.Text.Json;
 using Hangfire;
+
+public class JobDescriptor(
+    string typeName,
+    string methodName,
+    IDictionary<string, object>? parameters = null
+)
+{
+    public string TypeName { get; } = typeName;
+    public string MethodName { get; } = methodName;
+    public IDictionary<string, object>? Parameters { get; } = parameters;
+}
 
 public class HangfireDynamicScheduler(IBackgroundJobClient client)
 {
-    public string EnqueueByTypeName(string typeName, string methodName)
+    private static readonly JsonSerializerOptions JsonSerializerOptions = new()
     {
-        // Find the type in the current assembly
-        var type =
-            Assembly.GetExecutingAssembly().GetType(typeName)
-            ?? throw new InvalidOperationException($"Type '{typeName}' not found.");
+        PropertyNameCaseInsensitive = false,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    };
 
-        // Find the method (must be instance, parameterless, returns Task)
+    /// <summary>
+    /// Enqueues a job based on the provided <see cref="JobDescriptor"/>.
+    /// </summary>
+    public string Enqueue(JobDescriptor descriptor, Assembly? assembly = null)
+    {
+        ArgumentNullException.ThrowIfNull(descriptor);
+        assembly ??= Assembly.GetExecutingAssembly();
+        var type =
+            assembly.GetType(descriptor.TypeName)
+            ?? throw new InvalidOperationException($"Type '{descriptor.TypeName}' not found.");
+
+        return type.IsInterface
+            ? this.EnqueueByInterfaceName(type, descriptor.MethodName, descriptor.Parameters)
+            : this.EnqueueByTypeName(type, descriptor.MethodName, descriptor.Parameters);
+    }
+
+    private string EnqueueByInterfaceName(
+        Type iface,
+        string methodName,
+        IDictionary<string, object>? parameters
+    )
+    {
+        // Find the correct method overload based on parameter types
         var method =
-            type.GetMethod(
-                methodName,
-                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic
-            )
+            FindMethodByNameAndParameters(iface, methodName, parameters)
             ?? throw new InvalidOperationException(
-                $"Method '{methodName}' not found on type '{typeName}'."
+                $"Method '{methodName}' not found on '{iface.FullName}' with matching parameters."
             );
 
-        // Create an instance of the type
-        var instance = System.Runtime.Serialization.FormatterServices.GetUninitializedObject(type);
+        var param = Expression.Parameter(iface, "x");
+        var methodParams = method.GetParameters();
+        var arguments = BuildMethodArguments(methodParams, parameters);
+        var call = Expression.Call(param, method, arguments);
 
-        // Build Expression<Func<T, Task>> for Hangfire
-        var param = Expression.Parameter(type, "x");
-        var call = Expression.Call(param, method);
-
-        Type lambdaType;
-        if (method.ReturnType == typeof(Task))
-        {
-            lambdaType = typeof(Func<,>).MakeGenericType(type, typeof(Task));
-        }
-        else if (method.ReturnType == typeof(void))
-        {
-            lambdaType = typeof(Action<>).MakeGenericType(type);
-        }
-        else
-        {
-            throw new InvalidOperationException("Method must return void or Task.");
-        }
-
+        var lambdaType = typeof(Func<,>).MakeGenericType(iface, typeof(Task));
         var lambda = Expression.Lambda(lambdaType, call, param);
 
-        // Use Hangfire's generic Enqueue<T>
-        var enqueueMethod = typeof(BackgroundJobClientExtensions)
+        var enqueueMethod = GetEnqueueMethod();
+        var genericEnqueue = enqueueMethod.MakeGenericMethod(iface);
+        return (string)(
+            genericEnqueue.Invoke(null, [client, lambda])
+            ?? throw new InvalidOperationException("Failed to enqueue job")
+        );
+    }
+
+    private static MethodInfo? FindMethodByNameAndParameters(
+        Type type,
+        string methodName,
+        IDictionary<string, object>? parameters
+    )
+    {
+        // Get all methods with the given name
+        var candidateMethods = type.GetMethods(
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic
+            )
+            .Where(m => m.Name == methodName)
+            .ToList();
+
+        if (candidateMethods.Count == 0)
+        {
+            return null;
+        }
+
+        // If only one method with this name exists, return it
+        if (candidateMethods.Count == 1)
+        {
+            return candidateMethods[0];
+        }
+
+        // If we have parameters, try to find the best match by parameter names
+        if (parameters != null && parameters.Count > 0)
+        {
+            var paramNames = parameters.Keys.ToHashSet(StringComparer.OrdinalIgnoreCase);
+
+            // Find methods where all parameters have matching names
+            var matchingMethods = candidateMethods
+                .Where(m =>
+                {
+                    var methodParamNames = m.GetParameters()
+                        .Select(p => p.Name)
+                        .Where(n => n != null)
+                        .ToHashSet(StringComparer.OrdinalIgnoreCase);
+
+                    // Check if all required parameters (non-optional) have values
+                    var requiredParams = m.GetParameters()
+                        .Where(p => !p.HasDefaultValue)
+                        .Select(p => p.Name)
+                        .Where(n => n != null);
+
+                    return requiredParams.All(rp => rp != null && paramNames.Contains(rp));
+                })
+                .ToList();
+
+            if (matchingMethods.Count == 1)
+            {
+                return matchingMethods[0];
+            }
+
+            // If multiple methods match, select the one with the most matching parameters
+            if (matchingMethods.Count > 1)
+            {
+                return matchingMethods
+                    .OrderByDescending(m =>
+                    {
+                        var methodParamNames = m.GetParameters()
+                            .Select(p => p.Name)
+                            .Where(n => n != null);
+
+                        return methodParamNames.Count(mpn =>
+                            mpn != null && paramNames.Contains(mpn)
+                        );
+                    })
+                    .FirstOrDefault();
+            }
+        }
+
+        // If we can't find by parameter names, just return the first method
+        return candidateMethods.FirstOrDefault();
+    }
+
+    private static Expression[] BuildMethodArguments(
+        ParameterInfo[] methodParams,
+        IDictionary<string, object>? parameters
+    )
+    {
+        var arguments = new Expression[methodParams.Length];
+        for (var i = 0; i < methodParams.Length; i++)
+        {
+            var methodParam = methodParams[i];
+            if (parameters != null && parameters.TryGetValue(methodParam.Name!, out var value))
+            {
+                var convertedValue = value;
+                if (value is JsonElement jsonElement)
+                {
+                    convertedValue = GetValue(methodParam, jsonElement);
+                }
+                arguments[i] = Expression.Constant(convertedValue, methodParam.ParameterType);
+            }
+            else if (methodParam.HasDefaultValue)
+            {
+                arguments[i] = Expression.Constant(
+                    methodParam.DefaultValue,
+                    methodParam.ParameterType
+                );
+            }
+            else
+            {
+                throw new ArgumentException(
+                    $"Required parameter '{methodParam.Name}' was not provided"
+                );
+            }
+        }
+        return arguments;
+    }
+
+    private static object? GetValue(ParameterInfo methodParam, JsonElement jsonElement) =>
+        jsonElement.ValueKind switch
+        {
+            JsonValueKind.String => jsonElement.GetString(),
+            JsonValueKind.Number when methodParam.ParameterType == typeof(int) =>
+                jsonElement.GetInt32(),
+            JsonValueKind.Number when methodParam.ParameterType == typeof(long) =>
+                jsonElement.GetInt64(),
+            JsonValueKind.Number when methodParam.ParameterType == typeof(double) =>
+                jsonElement.GetDouble(),
+            JsonValueKind.True or JsonValueKind.False => jsonElement.GetBoolean(),
+            JsonValueKind.Object => JsonSerializer.Deserialize(
+                jsonElement.GetRawText(),
+                methodParam.ParameterType,
+                JsonSerializerOptions
+            ),
+            JsonValueKind.Array => JsonSerializer.Deserialize(
+                jsonElement.GetRawText(),
+                methodParam.ParameterType,
+                JsonSerializerOptions
+            ),
+            _ => throw new ArgumentException(
+                $"Unsupported parameter type '{methodParam.ParameterType}' for '{methodParam.Name}'"
+            ),
+        };
+
+    private static MethodInfo GetEnqueueMethod()
+    {
+        return typeof(BackgroundJobClientExtensions)
             .GetMethods()
             .First(m =>
             {
@@ -63,10 +222,34 @@ public class HangfireDynamicScheduler(IBackgroundJobClient client)
                     && inner.GetGenericTypeDefinition() == typeof(Func<,>)
                     && inner.GetGenericArguments()[1] == typeof(Task);
             });
+    }
 
+    private string EnqueueByTypeName(
+        Type type,
+        string methodName,
+        IDictionary<string, object>? parameters
+    )
+    {
+        // Find the correct method overload based on parameter types
+        var method =
+            FindMethodByNameAndParameters(type, methodName, parameters)
+            ?? throw new InvalidOperationException(
+                $"Method '{methodName}' not found on type '{type.FullName}' with matching parameters."
+            );
+
+        var param = Expression.Parameter(type, "x");
+        var methodParams = method.GetParameters();
+        var arguments = BuildMethodArguments(methodParams, parameters);
+        var call = Expression.Call(param, method, arguments);
+
+        var lambdaType = typeof(Func<,>).MakeGenericType(type, typeof(Task));
+        var lambda = Expression.Lambda(lambdaType, call, param);
+
+        var enqueueMethod = GetEnqueueMethod();
         var genericEnqueue = enqueueMethod.MakeGenericMethod(type);
-
-        // Call Enqueue<T>(client, Expression<Func<T, Task>>)
-        return (string)genericEnqueue.Invoke(null, [client, lambda]);
+        return (string)(
+            genericEnqueue.Invoke(null, [client, lambda])
+            ?? throw new InvalidOperationException("Failed to enqueue job")
+        );
     }
 }
--- samples/Web/Program.cs
+++ samples/Web/Program.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using Hangfire;
 using Web;
 
@@ -5,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.AddHangfireServer();
 builder.Services.AddSingleton(TimeProvider.System);
 builder.Services.AddTransient<TimeJob>();
+builder.Services.AddTransient<ITimeJob, TimeJob>();
+builder.Services.AddTransient<ITimeJobWithParameters, TimeJob>();
 builder.Services.AddTransient<HangfireDynamicScheduler>();
 builder.Services.AddProblemDetails();
 builder.Services.AddControllers();
@@ -15,7 +18,7 @@ app.MapPost(
     "/jobs",
     (JobDescriptor jobDescriptor, HangfireDynamicScheduler scheduler) =>
     {
-        var jobId = scheduler.EnqueueByTypeName(jobDescriptor.TypeName, jobDescriptor.MethodName);
+        var jobId = scheduler.Enqueue(jobDescriptor);
 
         return Results.Ok($"Job executed successfully with ID: {jobId}");
     }
@@ -25,9 +28,12 @@ app.MapGet(
     "/timeJob",
     (HangfireDynamicScheduler scheduler) =>
     {
-        var jobId = scheduler.EnqueueByTypeName(
-            typeof(TimeJob).FullName!,
-            nameof(TimeJob.ExecuteAsync)
+        var jobId = scheduler.Enqueue(
+            new(
+                typeof(ITimeJob).FullName!,
+                nameof(ITimeJob.ExecuteAsync),
+                new Dictionary<string, object> { ["userName"] = "John Doe" }
+            )
         );
         return Results.Ok($"Job executed successfully with ID: {jobId}");
     }
@@ -37,7 +43,7 @@ app.MapGet(
     "/timeJobClient",
     (IBackgroundJobClient client) =>
     {
-        client.Enqueue<TimeJob>(x => x.ExecuteAsync());
+        client.Enqueue<TimeJob>(x => x.ExecuteAsync("John Doe"));
         return Results.Ok("Time job executed successfully.");
     }
 );
@@ -46,17 +52,54 @@ app.MapHangfireDashboard(string.Empty);
 
 app.Run();
 
-public class JobDescriptor
-{
-    public string TypeName { get; set; } = string.Empty;
-    public string MethodName { get; set; } = string.Empty;
-}
-
-public class TimeJob(TimeProvider timeProvider)
+public class TimeJob(TimeProvider timeProvider, ILogger<TimeJob> logger)
+    : ITimeJob,
+        ITimeJobWithParameters
 {
     public Task ExecuteAsync()
     {
-        Console.WriteLine($"Current time: {timeProvider.GetUtcNow():yyyy-MM-dd HH:mm:ss}");
+        logger.LogInformation(
+            "Current time: {CurrentTime}",
+            timeProvider.GetUtcNow().ToString("yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture)
+        );
         return Task.CompletedTask;
     }
+
+    public Task ExecuteAsync(string userName)
+    {
+        logger.LogInformation(
+            "Current time: {CurrentTime}, User: {UserName}",
+            timeProvider.GetUtcNow().ToString("yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture),
+            userName
+        );
+        return Task.CompletedTask;
+    }
+
+    public Task ExecuteAsync(Payload payload)
+    {
+        logger.LogInformation(
+            "Current time: {CurrentTime}, User: {UserName}, Age: {Age}",
+            timeProvider.GetUtcNow().ToString("yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture),
+            payload.UserName,
+            payload.Age
+        );
+        return Task.CompletedTask;
+    }
+}
+
+public interface ITimeJob
+{
+    public Task ExecuteAsync();
+}
+
+public interface ITimeJobWithParameters
+{
+    public Task ExecuteAsync(Payload payload);
+    public Task ExecuteAsync(string userName);
+}
+
+public class Payload
+{
+    public string UserName { get; set; } = string.Empty;
+    public int Age { get; set; }
 }
